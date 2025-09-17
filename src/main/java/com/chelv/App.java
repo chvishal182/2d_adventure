d53731675ce2ec356e425bf7f96c63caed86bd74
cvishaldev <cvishaldev@gmail.com>
2025-09-17 18:52:36 +0530

    Added Real world tiles such as grass, water and walls

--- src/main/java/com/chelv/App.java
+++ src/main/java/com/chelv/App.java
@@ -4,17 +4,10 @@ import javax.swing.JFrame;
 
 import com.chelv.panel.GamePanel;
 
-/**
- * Hello world!
- */
 public final class App {
     private App() {
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
         
         JFrame window = new JFrame();
